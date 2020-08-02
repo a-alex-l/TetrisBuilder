6d55bb21e1cd2c40a675655ae35e78d46bf9dbfa
a-alex-l <lesnik-a-a@yandex.ru>
2020-08-02 23:08:16 +0300

    Merging GamaPlay and Interface

--- TetrisBuilder/UI/Levels/Basic.gd
+++ TetrisBuilder/UI/Levels/Basic.gd
@@ -17,42 +17,42 @@ func _process(delta):
 
 func _show_all():
 	$MarginContainer.show()
-	$GameUI.show()
+	$Zero/Camera2D/GameUI.show()
 	$Zero.show()
-	$Turns.show()
+	$Zero/Camera2D/Turns.show()
 
 func _hide_all():
 	$Zero.hide()
-	$GameUI.hide()
-	$Turns.hide()
+	$Zero/Camera2D/GameUI.hide()
+	$Zero/Camera2D/Turns.hide()
 	$MarginContainer.hide()
 
 func _start_game(scene_to_load):
 	$Zero._create_level(scene_to_load)
-	$GameUI._ready()
-	$GameUI.animated_blocks = $Zero.get_count_of_remaining_blocks()
+	$Zero/Camera2D/GameUI._ready()
+	$Zero/Camera2D/GameUI.animated_blocks = $Zero.get_count_of_remaining_blocks()
 
 func _update_all():
 	var blocks = $Zero.get_count_of_remaining_blocks() #сколько осталось (инт)
 	var failed = $Zero.get_count_fallen_blocks() #сколько выпало за сцену
 	var score = $Zero.get_score_now() #очки
-	$GameUI.update_health($GameUI.player_max_health - failed)
-	$GameUI.update_score(score)
-	$GameUI.update_blocks(blocks)
+	$Zero/Camera2D/GameUI.update_health($Zero/Camera2D/GameUI.player_max_health - failed)
+	$Zero/Camera2D/GameUI.update_score(score)
+	$Zero/Camera2D/GameUI.update_blocks(blocks)
 	
 func _check_turns():
-	if $Turns.condition == '1' :
+	if $Zero/Camera2D/Turns.condition == '1' :
 		$Zero.turn_kinematic_left()
-		$Turns.condition = '0'
-	if $Turns.condition == '2' :
+		$Zero/Camera2D/Turns.condition = '0'
+	if $Zero/Camera2D/Turns.condition == '2' :
 		$Zero.turn_kinematic_right()
-		$Turns.condition = '0'
+		$Zero/Camera2D/Turns.condition = '0'
 
 func _check_end():
-	if round($GameUI.animated_health) == 0:
+	if round($Zero/Camera2D/GameUI.animated_health) == 0:
 		_hide_all()
 		$LoseScene.show()
-	if $GameUI.animated_blocks == 0:
+	if $Zero/Camera2D/GameUI.animated_blocks == 0:
 		_hide_all()
 		$WinScene.show()
 
