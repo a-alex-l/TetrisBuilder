4aeda01113056d9be6ada2a0e74d1b30b2ce54c0
a-alex-l <lesnik-a-a@yandex.ru>
2020-11-07 01:23:03 +0300

    Added AdMob. NOT TESTED

--- TetrisBuilder/scripts/MenuWindowControl.gd
+++ TetrisBuilder/scripts/MenuWindowControl.gd
@@ -1,23 +1,6 @@
 extends Control
 
-var AdMob
-var AdMobDict = Dictionary()
-
-var GodotAds
-
-func _init():
-	return
-	GodotAds = Engine.get_singleton("GodotAds")
-	GodotAds.init(get_instance_id())
-	AdMob = GodotAds.get_singleton("AdMob")
-	AdMobDict["BannerAd"] = false
-	AdMobDict["InterstitialAd"] = false
-	AdMobDict["RewardedVideoAd"] = true
-	AdMobDict["BannerGravity"] = "BOTTOM"
-	AdMobDict["BannerAdId"] = "your banner ad id"
-	AdMobDict["InterstitialAdId"] = "your interstitial ad id"
-	AdMobDict["RewardedVideoAdId"] = "rewarded video ad id"
-
+var is_rewarded_video_loaded = false
 
 func _on_PlayButton_pressed():
 	get_parent().replace_scene(load("res://scenes/Levels.tscn").instance())
@@ -26,6 +9,12 @@ func _on_SettingsButton_pressed():
 	get_parent().replace_scene(load("res://scenes/Settings.tscn").instance())
 
 func _on_SupportUsButton_pressed():
-	return
-	if (AdMob.is_rewarded_video_loaded()):
-		AdMob.show_rewarded_video()
+	if (is_rewarded_video_loaded):
+		$AdMob.show_rewarded_video()
+
+func _on_AdMob_rewarded(currency, ammount):
+	is_rewarded_video_loaded = false
+	$AdMob.load_rewarded_video()
+
+func _on_AdMob_rewarded_video_loaded():
+	is_rewarded_video_loaded = true
